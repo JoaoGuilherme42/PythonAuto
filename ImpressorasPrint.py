3d56866ce97c7532ee101a168d2da852d2d86911
João Guilherme Abrão Braga <109540210+JoaoGuilherme42@users.noreply.github.com>
2022-08-16 15:06:12 -0400

    finalComit3

--- ImpressorasPrint.py
+++ ImpressorasPrint.py
@@ -3,77 +3,86 @@ import time
 import os
 import webbrowser
 
-num1 = int(pyautogui.prompt(text='Você deseja tirar um print de TODAS as impressoras?\nDigite 1 para SIM\nDigite 2 para NÃO',
+num1 = int(pyautogui.prompt(text='Você deseja tirar um print de TODAS as impressoras por contrato ou individual?\nDigite 1 para TODAS\nDigite 2 para INDIVIDUAL',
            title='Developed by JoaoGuilherme'))
 
 if(num1 == 1):
-    webbrowser.open('http://192.168.0.210/#hId-pgDevInfo')
-    pyautogui.press('', interval=5)
-    dptoScreen = pyautogui.screenshot()
-    dptoScreen.save(
-        r'.\PrintsImpressoras\Numero de serie e contador da impressora do Departamento Pessoal.png')
-    webbrowser.open('http://192.168.0.211/#hId-pgDevInfo')
-    pyautogui.press('', interval=5)
-    recepScreen = pyautogui.screenshot()
-    recepScreen.save(
-        r'.\PrintsImpressoras\Numero de serie e contador da impressora da Recepção do Recanto.png')
-    webbrowser.open('http://192.168.0.213/#hId-pgDevInfo')
-    pyautogui.press('', interval=5)
-    farmaScreen = pyautogui.screenshot()
-    farmaScreen.save(
-        r'.\PrintsImpressoras\Numero de serie e contador da impressora da Farmácia.png')
-    webbrowser.open('http://192.168.0.214/#hId-pgDevInfo')
-    pyautogui.press('', interval=5)
-    ambuScreen = pyautogui.screenshot()
-    ambuScreen.save(
-        r'.\PrintsImpressoras\Numero de serie e contador da impressora do Ambulatório.png')
-    webbrowser.open('http://192.168.0.215/#hId-pgDevInfo')
-    pyautogui.press('', interval=5)
-    chefScreen = pyautogui.screenshot()
-    chefScreen.save(
-        r'.\PrintsImpressoras\Numero de serie e contador da impressora da Chefia de Enfermagem.png')
-    webbrowser.open('http://192.168.0.220/#hId-pgDevInfo')
-    pyautogui.press('', interval=5)
-    finanScreen = pyautogui.screenshot()
-    finanScreen.save(
-        r'.\PrintsImpressoras\Numero de serie e contador da impressora do Financeiro.png')
-    webbrowser.open('http://192.168.0.221/#hId-pgDevInfo')
-    pyautogui.press('', interval=5)
-    psicoScreen = pyautogui.screenshot()
-    psicoScreen.save(
-        r'.\PrintsImpressoras\Numero de serie e contador da impressora da Psicologia SUS.png')
-    webbrowser.open('http://192.168.0.222/#hId-pgDevInfo')
-    pyautogui.press('', interval=5)
-    eletroScreen = pyautogui.screenshot()
-    eletroScreen.save(
-        r'.\PrintsImpressoras\Numero de serie e contador da impressora do Eletroencefalograma.png')
-    webbrowser.open('http://192.168.0.223/#hId-pgDevInfo')
-    pyautogui.press('', interval=5)
-    almoxScreen = pyautogui.screenshot()
-    almoxScreen.save(
-        r'.\PrintsImpressoras\Numero de serie e contador da impressora do Almoxarifado.png')
-    webbrowser.open('http://192.168.0.224/#hId-pgDevInfo')
-    pyautogui.press('', interval=5)
-    hospdScreen = pyautogui.screenshot()
-    hospdScreen.save(
-        r'.\PrintsImpressoras\Numero de serie e contador da impressora do Hospital Dia.png')
-    webbrowser.open('http://192.168.0.128/general/information.html?kind=item')
-    pyautogui.press('', interval=5)
-    hospdScreen = pyautogui.screenshot()
-    hospdScreen.save(
-        r'.\PrintsImpressoras\Numero de serie e contador da impressora da Ala Feminina SUS.png')
-    webbrowser.open('http://192.168.0.131/general/information.html?kind=item')
-    pyautogui.press('', interval=5)
-    hospdScreen = pyautogui.screenshot()
-    hospdScreen.save(
-        r'.\PrintsImpressoras\Numero de serie e contador da impressora da Ala Masculina SUS.png')
-    webbrowser.open('http://192.168.0.178/sws/index.html')
-    pyautogui.press('', interval=5)
-    pyautogui.click(x=360, y=175, interval=2)
-    pyautogui.click(x=100, y=335, interval=2)
-    hospdScreen = pyautogui.screenshot()
-    hospdScreen.save(
-        r'.\PrintsImpressoras\Numero de serie e contador da impressora do Dr Antonio.png')
+    num2 = int(pyautogui.prompt(text='Digite 1 para o contrato da H2L\nDigite 2 para o contrato da COPIER',
+                                title='Developed by JoaoGuilherme'))
+    if(num2 == 1):
+        webbrowser.open('http://192.168.0.210/#hId-pgDevInfo')
+        pyautogui.press('', interval=5)
+        dptoScreen = pyautogui.screenshot()
+        dptoScreen.save(
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora do Departamento Pessoal.png')
+        webbrowser.open('http://192.168.0.211/#hId-pgDevInfo')
+        pyautogui.press('', interval=5)
+        recepScreen = pyautogui.screenshot()
+        recepScreen.save(
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora da Recepção do Recanto.png')
+        webbrowser.open('http://192.168.0.213/#hId-pgDevInfo')
+        pyautogui.press('', interval=5)
+        farmaScreen = pyautogui.screenshot()
+        farmaScreen.save(
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora da Farmácia.png')
+        webbrowser.open('http://192.168.0.214/#hId-pgDevInfo')
+        pyautogui.press('', interval=5)
+        ambuScreen = pyautogui.screenshot()
+        ambuScreen.save(
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora do Ambulatório.png')
+        webbrowser.open('http://192.168.0.215/#hId-pgDevInfo')
+        pyautogui.press('', interval=5)
+        chefScreen = pyautogui.screenshot()
+        chefScreen.save(
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora da Chefia de Enfermagem.png')
+        webbrowser.open('http://192.168.0.220/#hId-pgDevInfo')
+        pyautogui.press('', interval=5)
+        finanScreen = pyautogui.screenshot()
+        finanScreen.save(
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora do Financeiro.png')
+        webbrowser.open('http://192.168.0.221/#hId-pgDevInfo')
+        pyautogui.press('', interval=5)
+        psicoScreen = pyautogui.screenshot()
+        psicoScreen.save(
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora da Psicologia SUS.png')
+        webbrowser.open('http://192.168.0.222/#hId-pgDevInfo')
+        pyautogui.press('', interval=5)
+        eletroScreen = pyautogui.screenshot()
+        eletroScreen.save(
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora do Eletroencefalograma.png')
+        webbrowser.open('http://192.168.0.223/#hId-pgDevInfo')
+        pyautogui.press('', interval=5)
+        almoxScreen = pyautogui.screenshot()
+        almoxScreen.save(
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora do Almoxarifado.png')
+        webbrowser.open('http://192.168.0.224/#hId-pgDevInfo')
+        pyautogui.press('', interval=5)
+        hospdScreen = pyautogui.screenshot()
+        hospdScreen.save(
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora do Hospital Dia.png')
+    elif(num2 == 2):
+        webbrowser.open(
+            'http://192.168.0.128/general/information.html?kind=item')
+        pyautogui.press('', interval=5)
+        hospdScreen = pyautogui.screenshot()
+        hospdScreen.save(
+            r'.\PrintsImpressoras\Copier\Numero de serie e contador da impressora da Ala Feminina SUS.png')
+        webbrowser.open(
+            'http://192.168.0.131/general/information.html?kind=item')
+        pyautogui.press('', interval=5)
+        hospdScreen = pyautogui.screenshot()
+        hospdScreen.save(
+            r'.\PrintsImpressoras\Copier\Numero de serie e contador da impressora da Ala Masculina SUS.png')
+        webbrowser.open('http://192.168.0.178/sws/index.html')
+        pyautogui.press('', interval=5)
+        pyautogui.click(x=360, y=175, interval=2)
+        pyautogui.click(x=100, y=335, interval=2)
+        hospdScreen = pyautogui.screenshot()
+        hospdScreen.save(
+            r'.\PrintsImpressoras\Copier\Numero de serie e contador da impressora do Dr Antonio.png')
+    else:
+        pyautogui.alert(
+            text='Você digitou um número inválido\nFeche o programa e inicie novamente', title='Developed by JoaoGuilherme', button='Ok')
 elif(num1 == 2):
     num = int(pyautogui.prompt(text='Você escolheu tirar o print de UMA impressora, escolha:\nDepartamento Pessoal: 1 \nRecepcao Recanto: 2 \nFarmacia: 3\nAmbulatório: 4\nChefia da Enfermagem: 5\nFinanceiro: 6\nPsicologia SUS: 7\nEletroencefalograma: 8\nAlmoxarifado: 9\nHospital Dia: 10\nSUS Feminino: 11\nSUS Masculino: 12\nDr. Antônio: 13', title='Developed by JoaoGuilherme', default='0'))
     if(num == 1):
@@ -81,75 +90,75 @@ elif(num1 == 2):
         pyautogui.press('', interval=3)
         dptoScreen = pyautogui.screenshot()
         dptoScreen.save(
-            r'.\PrintsImpressoras\Numero de serie e contador da impressora do Departamento Pessoal.png')
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora do Departamento Pessoal.png')
     elif(num == 2):
         webbrowser.open('http://192.168.0.211/#hId-pgDevInfo')
         pyautogui.press('', interval=3)
         recepScreen = pyautogui.screenshot()
         recepScreen.save(
-            r'.\PrintsImpressoras\Numero de serie e contador da impressora da Recepção do Recanto.png')
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora da Recepção do Recanto.png')
     elif(num == 3):
         webbrowser.open('http://192.168.0.213/#hId-pgDevInfo')
         pyautogui.press('', interval=3)
         farmaScreen = pyautogui.screenshot()
         farmaScreen.save(
-            r'.\PrintsImpressoras\Numero de serie e contador da impressora da Farmácia.png')
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora da Farmácia.png')
     elif(num == 4):
         webbrowser.open('http://192.168.0.214/#hId-pgDevInfo')
         pyautogui.press('', interval=3)
         ambuScreen = pyautogui.screenshot()
         ambuScreen.save(
-            r'.\PrintsImpressoras\Numero de serie e contador da impressora do Ambulatório.png')
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora do Ambulatório.png')
     elif(num == 5):
         webbrowser.open('http://192.168.0.215/#hId-pgDevInfo')
         pyautogui.press('', interval=3)
         chefScreen = pyautogui.screenshot()
         chefScreen.save(
-            r'.\PrintsImpressoras\Numero de serie e contador da impressora da Chefia de Enfermagem.png')
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora da Chefia de Enfermagem.png')
     elif(num == 6):
         webbrowser.open('http://192.168.0.220/#hId-pgDevInfo')
         pyautogui.press('', interval=3)
         finanScreen = pyautogui.screenshot()
         finanScreen.save(
-            r'.\PrintsImpressoras\Numero de serie e contador da impressora do Financeiro.png')
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora do Financeiro.png')
     elif(num == 7):
         webbrowser.open('http://192.168.0.221/#hId-pgDevInfo')
         pyautogui.press('', interval=3)
         psicoScreen = pyautogui.screenshot()
         psicoScreen.save(
-            r'.\PrintsImpressoras\Numero de serie e contador da impressora da Psicologia SUS.png')
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora da Psicologia SUS.png')
     elif(num == 8):
         webbrowser.open('http://192.168.0.222/#hId-pgDevInfo')
         pyautogui.press('', interval=3)
         eletroScreen = pyautogui.screenshot()
         eletroScreen.save(
-            r'.\PrintsImpressoras\Numero de serie e contador da impressora do Eletroencefalograma.png')
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora do Eletroencefalograma.png')
     elif(num == 9):
         webbrowser.open('http://192.168.0.223/#hId-pgDevInfo')
         pyautogui.press('', interval=3)
         almoxScreen = pyautogui.screenshot()
         almoxScreen.save(
-            r'.\PrintsImpressoras\Numero de serie e contador da impressora do Almoxarifado.png')
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora do Almoxarifado.png')
     elif(num == 10):
         webbrowser.open('http://192.168.0.224/#hId-pgDevInfo')
         pyautogui.press('', interval=3)
         hospdScreen = pyautogui.screenshot()
         hospdScreen.save(
-            r'.\PrintsImpressoras\Numero de serie e contador da impressora do Hospital Dia.png')
+            r'.\PrintsImpressoras\H2L\Numero de serie e contador da impressora do Hospital Dia.png')
     elif(num == 11):
         webbrowser.open(
             'http://192.168.0.128/general/information.html?kind=item')
         pyautogui.press('', interval=3)
         hospdScreen = pyautogui.screenshot()
         hospdScreen.save(
-            r'.\PrintsImpressoras\Numero de serie e contador da impressora da Ala Feminina SUS.png')
+            r'.\PrintsImpressoras\Copier\Numero de serie e contador da impressora da Ala Feminina SUS.png')
     elif(num == 12):
         webbrowser.open(
             'http://192.168.0.131/general/information.html?kind=item')
         pyautogui.press('', interval=3)
         hospdScreen = pyautogui.screenshot()
         hospdScreen.save(
-            r'.\PrintsImpressoras\Numero de serie e contador da impressora da Ala Masculina SUS.png')
+            r'.\PrintsImpressoras\Copier\Numero de serie e contador da impressora da Ala Masculina SUS.png')
     elif(num == 13):
         webbrowser.open('http://192.168.0.178/sws/index.html')
         pyautogui.press('', interval=3)
@@ -157,7 +166,7 @@ elif(num1 == 2):
         pyautogui.click(x=100, y=335, interval=1)
         hospdScreen = pyautogui.screenshot()
         hospdScreen.save(
-            r'.\PrintsImpressoras\Numero de serie e contador da impressora do Dr Antonio.png')
+            r'.\PrintsImpressoras\Copier\Numero de serie e contador da impressora do Dr Antonio.png')
     else:
         pyautogui.alert(
             text='Você digitou um número inválido\nFeche o programa e inicie novamente', title='Developed by JoaoGuilherme', button='Ok')
